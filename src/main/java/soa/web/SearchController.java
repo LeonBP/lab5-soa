50385ef080ee52eec056658f028a1528ecdd672e
LeonBP <leonbarbed@gmail.com>
2017-12-19 17:19:49 +0100

    Práctica acabada

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+import java.util.HashMap;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +26,24 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        String delims = "[ ]+";
+        String[] tokens = q.split(delims);
+        String keywords = "";
+        int max = 50;
+        for(int i = 0; i < tokens.length; i++){
+           if(tokens[i].startsWith("max:")){
+              try{
+                  max = Integer.parseInt(tokens[i].substring(tokens[i].indexOf(":")+1));
+                  continue;
+              }catch(NumberFormatException e){}
+           }
+           keywords += tokens[i] + " ";
+        }
+        
+        Map headers = new HashMap<>();
+        headers.put("CamelTwitterKeywords", keywords);
+        headers.put("CamelTwitterCount", max);
+        
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
